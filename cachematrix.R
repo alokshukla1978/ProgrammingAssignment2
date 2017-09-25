c2b35ca7bf298095fb234be62167c56e4ee2f1d8
Alok Shukla <alok.kshukla@gmail.com>
2017-09-24 21:24:19 -0700

    Commit of camchematrix.R as per assignment requirements

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This function provides cacheMatrix object and associated getter/ 
+# setter functions. This could be used to store the cached value
+# get() would return the source matrix object
+# set() would set the matrix object with input value
+# getinverse() would return the stored inverse matrix object
+# setinverse() would store the calculated inverse matrix object
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(sMatrix = matrix()) {
+        rMatrix <- NULL
+        set <- function(y) {
+          sMatrix <<- y
+          rMatrix <<- NULL
+        }
+        get <- function() sMatrix
+        setinverse <- function(revMatrix) rMatrix <<- revMatrix
+        getinverse <- function() rMatrix
+        list(set = set, 
+             get = get, 
+             setinverse = setinverse, 
+             getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
+# This function calculates inverse of matrix. However, it would return
+# the cached value if
+# 1) a cache value of inverse already exists due to previous request
+# 2) the original source matrix has not changed
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+        }
+        sourceMatrix <- x$get()
+        m <- solve(sourceMatrix)
+        x$setinverse(m)
+        m
 }
